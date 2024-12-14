39c5c78d03bff8be034837638967109ae2dc6f11
Tim Beattie <trbeattie@outlook.com>
2024-12-14 16:53:57 -0500

    Fix various pylint errors

--- kotoba_api/app.py
+++ kotoba_api/app.py
@@ -1,3 +1,5 @@
+"""Root module of kotoba-api"""
+
 from fastapi import FastAPI
 
 from .config import Settings
@@ -13,4 +15,5 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """Responds at the root path with a placeholder message"""
     return "Hello, world!"
--- kotoba_api/util.py
+++ kotoba_api/util.py
@@ -1,3 +1,5 @@
+"""Provides various utility methods utilized by kotoba-api"""
+
 import os
 from pathlib import Path
 
@@ -7,14 +9,14 @@ import requests
 
 def download_file(url: HttpUrl, destination_dir: str):
     """Handler for downloading large files over HTTP"""
-    file_basename = str(url).split("/")[-1]
+    file_basename = str(url).rsplit("/", maxsplit=1)[-1]
     destination_filename = os.path.join(destination_dir, file_basename)
 
     # Ensure destination download directory exists
     Path(destination_dir).mkdir(parents=True, exist_ok=True)
 
     # Stream remote file to destination path
-    with requests.get(url, stream=True) as response:
+    with requests.get(url, stream=True, timeout=10) as response:
         response.raise_for_status()
 
         with open(destination_filename, "wb") as file_buffer:
