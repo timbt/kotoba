ac86d3ebe31a197b8d3f705a9fd78520fbb863ea
Tim Beattie <trbeattie@outlook.com>
2024-12-14 16:05:03 -0500

    Move download_file method into new util module

---
+++ kotoba_api/util.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from pydantic import HttpUrl
+import requests
+
+
+def download_file(url: HttpUrl, destination_dir: str):
+    """Handler for downloading large files over HTTP"""
+    file_basename = str(url).split("/")[-1]
+    destination_filename = os.path.join(destination_dir, file_basename)
+
+    # Ensure destination download directory exists
+    Path(destination_dir).mkdir(parents=True, exist_ok=True)
+
+    # Stream remote file to destination path
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+
+        with open(destination_filename, "wb") as file_buffer:
+            for chunk in response.iter_content(chunk_size=8192):
+                file_buffer.write(chunk)
+
+    return destination_filename
