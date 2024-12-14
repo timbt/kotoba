cf011447f7a8e645ef85923d897ff4b8f416b2ea
Tim Beattie <trbeattie@outlook.com>
2024-12-14 17:01:27 -0500

    Add docstring documentation in config.py

--- kotoba_api/config.py
+++ kotoba_api/config.py
@@ -1,9 +1,13 @@
+"""Provides settings classes for kotoba-api"""
+
 from pydantic import HttpUrl
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    """Provides default and environment variable configuration values for kotoba-api"""
+
     dict_uri: HttpUrl = "http://ftp.edrdg.org/pub/Nihongo/JMdict_e.gz"
     dict_download_dir: str = "data"
 
